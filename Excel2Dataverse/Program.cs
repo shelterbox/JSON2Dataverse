a0b184b4abd726515ab0dd45da15f9d61cefb648
Carter Moorse <40695530+Carter-Moorse@users.noreply.github.com>
2024-08-28 11:52:45 +0100

    Load JSON, convert to metadata, send request

--- Excel2Dataverse/Program.cs
+++ Excel2Dataverse/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Configuration;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using ClosedXML.Excel;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.PowerPlatform.Dataverse.Client;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Messages;
 using Microsoft.Xrm.Sdk.Metadata;
@@ -15,9 +16,14 @@ namespace Excel2Dataverse
     [Required] public string? Name { get; set; }
     public Dictionary<string, string> Members { get; set; } = new();
 
-    public string getSchemaName()
+    public static string formatPrefix(string? prefix)
     {
-      return $"{Module}.{Name}".ToLower();
+      return prefix != null && prefix.Trim() != "" ? $"{prefix}_" : "";
+    }
+
+    public string getSchemaName(string? prefix)
+    {
+      return $"{formatPrefix(prefix)}{Module}.{Name}".Replace(".", "").ToLower();
     }
 
     public string getDisplayName()
@@ -30,16 +36,28 @@ namespace Excel2Dataverse
       return $"{Module}.{Name}s";
     }
 
-    public EntityMetadata generateEntity(int locale, string description)
+    public EntityMetadata generateEntity(int locale, string? prefix, string? description)
     {
       return new EntityMetadata
       {
-        SchemaName = getSchemaName(),
+        SchemaName = getSchemaName(prefix),
         DisplayName = new Label(getDisplayName(), locale),
         DisplayCollectionName = new Label(getDisplayCollectionName(), locale),
         Description = new Label(description, locale),
         OwnershipType = OwnershipTypes.UserOwned,
         IsActivity = false
+      };
+    }
+
+    public StringAttributeMetadata generatePrimaryAttribute(int locale, string? prefix)
+    {
+      return new StringAttributeMetadata()
+      {
+        SchemaName = $"{formatPrefix(prefix)}id",
+        DisplayName = new Label("ID", locale),
+        MaxLength = 100,
+        FormatName = StringFormatName.Text,
+        RequiredLevel = new AttributeRequiredLevelManagedProperty(AttributeRequiredLevel.SystemRequired)
       };
     }
   }
@@ -55,33 +73,136 @@ class Program
 
   static void Main()
   {
+    /* TODO -- Add paths to:
+                - Create entities (w/ .JSON)
+                - Create relationships (w/ .JSON)
+                - Uploade data (w/ .XLSX)
+
+    */
+
+    // Default parameters
     int defaultLocale = 2057;
+    string defaultPrefix = "OAP";
     string defaultDescription = "Imported from the Ops App.";
 
-    Console.WriteLine("Sign-in to your Microsoft Account ...\n\n");
-    // IOrganizationService service = new ServiceClient(connectionString());
-    // var whoAmI = (WhoAmIResponse)service.Execute(new WhoAmIRequest());
-    // Console.WriteLine($"User ID is {whoAmI.UserId}.\n\n");
 
-    Excel2Dataverse.JSONEntity testObject1 = new(){
-      Module = "Countries",
-      Name = "Country"
-    };
-    testObject1.Members.Add("Active", "Boolean");
-    testObject1.Members.Add("Capital", "String");
-    testObject1.Members.Add("Continent", "String");
+    // Login to Microsoft
+    Console.WriteLine("Sign-in to your Microsoft Account ...\n");
+    IOrganizationService service = new ServiceClient(connectionString());
+    var whoAmI = (WhoAmIResponse)service.Execute(new WhoAmIRequest());
+    Console.WriteLine($"User ID is {whoAmI.UserId}.\n");
 
-    string testJSON1 = JsonSerializer.Serialize(testObject1);
 
-    Console.WriteLine(testJSON1);
+    // Load entities JSON
+    List<Excel2Dataverse.JSONEntity>? jsonEntities = JsonSerializer.Deserialize<List<Excel2Dataverse.JSONEntity>>(@"[
+	{
+		""Module"": ""Countries"",
+		""Name"": ""Country"",
+		""Members"": {
+			""Active"": ""Boolean"",
+			""Capital"": ""String"",
+			""Continent"": ""String"",
+			""DS"": ""String"",
+			""EDGAR"": ""String"",
+			""FIFA"": ""String"",
+			""FIPS"": ""String"",
+			""GAUL"": ""String"",
+			""Geoname_ID"": ""Decimal"",
+			""hasCountryImage"": ""Boolean"",
+			""HealthRiskSeverity"": ""Enum"",
+			""ID"": ""Long"",
+			""ImageURL"": ""String"",
+			""Intermediate_Region_Code"": ""String"",
+			""Intermediate_Region_Name"": ""String"",
+			""IOC"": ""String"",
+			""ISO3166_1_Alpha_2"": ""String"",
+			""ISO3166_1_Alpha_3"": ""String"",
+			""ISO3166_1_Numeric"": ""String"",
+			""ITU"": ""String"",
+			""Latitude"": ""Decimal"",
+			""Longitude"": ""Decimal"",
+			""M49"": ""Decimal"",
+			""MARC"": ""String"",
+			""Name"": ""String"",
+			""Name_ar_Formal"": ""String"",
+			""Name_ar_Official"": ""String"",
+			""Name_ar_Short"": ""String"",
+			""Name_cn_Formal"": ""String"",
+			""Name_cn_Official"": ""String"",
+			""Name_cn_Short"": ""String"",
+			""Name_en_Formal"": ""String"",
+			""Name_en_Official"": ""String"",
+			""Name_en_Short"": ""String"",
+			""Name_es_Formal"": ""String"",
+			""Name_es_Official"": ""String"",
+			""Name_es_Short"": ""String"",
+			""Name_fr_Formal"": ""String"",
+			""Name_fr_Official"": ""String"",
+			""Name_fr_Short"": ""String"",
+			""Name_ru_Formal"": ""String"",
+			""Name_ru_Official"": ""String"",
+			""Name_ru_Short"": ""String"",
+			""Region_Code"": ""String"",
+			""Region_Name"": ""String"",
+			""Sub_region_Code"": ""String"",
+			""Sub_region_Name"": ""String"",
+			""TimeZones"": ""String"",
+			""TLD"": ""String"",
+			""WMO"": ""String""
+		}
+	},
+	{
+		""Module"": ""Countries"",
+		""Name"": ""CountryImage"",
+		""Members"": {
+			""changedDate"": ""DateTime"",
+			""Contents"": ""Binary"",
+			""createdDate"": ""DateTime"",
+			""DeleteAfterDownload"": ""Boolean"",
+			""FileID"": ""Auto number"",
+			""__FileName__"": ""Long"",
+			""HasContents"": ""Boolean"",
+			""ID"": ""Long"",
+			""Name"": ""String"",
+			""PublicThumbnailPath"": ""String"",
+			""Size"": ""Long"",
+			""__UUID__"": ""String""
+		}
+	}
+]");
 
-    Excel2Dataverse.JSONEntity? testObject2 = JsonSerializer.Deserialize<Excel2Dataverse.JSONEntity>(testJSON1);
+    if (jsonEntities == null)
+    {
+      Console.WriteLine("No JSON to load, potentially invalid JSON.");
+      return;
+    }
 
-    // Create entity
-    // CreateEntityRequest createrequest = new CreateEntityRequest();
+    Console.WriteLine($"{jsonEntities.Count} {(jsonEntities.Count == 1 ? "entity" : "entities")} loaded. Creating schemas ...");
 
-    // service.Execute(createrequest);
-    // Console.WriteLine("The bank account entity has been created.");
+
+    // Create entities
+    int count = 1;
+    foreach (Excel2Dataverse.JSONEntity jsonEntity in jsonEntities)
+    {
+      EntityMetadata dataverseEntity = jsonEntity.generateEntity(defaultLocale, defaultPrefix, defaultDescription);
+      StringAttributeMetadata dataversePrimaryAttribute = jsonEntity.generatePrimaryAttribute(defaultLocale, defaultPrefix);
+
+      CreateEntityRequest createrequest = new CreateEntityRequest()
+      {
+        Entity = dataverseEntity,
+        HasActivities = false,
+        HasFeedback = false,
+        HasNotes = false,
+        PrimaryAttribute = dataversePrimaryAttribute
+      };
+
+      service.Execute(createrequest);
+      Console.WriteLine($"{count}/{jsonEntities.Count} - {jsonEntity.getDisplayName()} entity schema has been created.");
+
+      count++;
+    }
+
+    // TODO -- Create relationships
 
     // Pause the console so it does not close.
     Console.WriteLine("Press the <Enter> key to exit.");
